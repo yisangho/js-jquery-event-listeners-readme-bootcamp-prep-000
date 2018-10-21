72f228d6514b67d4110e7ea7de362489fe748668
yisangho <yisangho717@gmail.com>
2018-10-21 03:48:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,41 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+
+frameIt();
+
+pressIt();
+
+submitIt();
+
 
 });
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71){
+      alert('You clicked G')
+    }
+  });
+}
+
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
